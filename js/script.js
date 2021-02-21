a77dafadae99a3663ec5987e0fc4faebf03ca564
Burence1 <burensomondi@gmail.com>
2021-02-21 20:24:46 +0300

    linked javascript to HTML

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 function getName() {
 
-  var date = new Date(document.querySelector('input[type="date"]').value)
+  var date = new Date(document.querySelector('input[type="date"]').value);
+  var maleChecked = document.getElementById('male').checked;
+  // var female = (document.getElementById('female').value);
 
-  var male = (document.querySelector('input[type="radio"]').value)
-  var female = (document.querySelector('input[type="radio"]').value)
+  console.log(male)
+  // console.log(female)
    gender = ''
   
-  if (gender == 'male'){
+  if (maleChecked){
     gender = 'male'
-  }
-  else {
+  }else {
     gender = 'female'
   }
   console.log(date,gender)
@@ -17,33 +18,14 @@ function getName() {
   var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
   
   var dayOfWeek = date.getDay();
-  //var date = new Date();
-  //date.setDate(date.getDate());
-
   var name ='';
   if (gender == 'male'){
     name = maleNames[dayOfWeek];
   }
-  else {
+else {
     name = femaleNames[dayOfWeek];
   }
   console.log('NAME IS ', name)
-   return name
+  document.getElementById("out").value = name;
   
-}
-// $(document).ready(function(){
-//       var date_input=$('input[name="date"]');
-//       var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-//       var options={
-//     format: 'mm/dd/yyyy',
-//         container: container,
-//         todayHighlight: true,
-//         autoclose: true,
-//       };
-//       date_input.datepicker(options);
-//     })
-
-// document.getElementById("#").innerHTML = maleNames;
-// document.getElementById("#").innerHTML = femaleNames;
-
-  
+}  
