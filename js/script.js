68ba76f43aeaf340f49463a9583fca205bc3b1f9
Burence1 <burensomondi@gmail.com>
2021-02-22 14:59:05 +0300

    added code comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
+/*declaring the getName function with no parameters*/
 function getName() {
 
   var date = new Date(document.querySelector('input[type="date"]').value);
   var maleChecked = document.getElementById('male').checked;
-    
-  console.log(male)
-   gender = ''
   
+    console.log(male);
+   gender = ''
+  /*using control flow to input data fom radio buttons*/
   if (maleChecked){
     gender = 'male'
   }else {
     gender = 'female'
   }
   console.log(date,gender)
+  /*putting Akan names in Arrays*/
   var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
   var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-      
+      /*getting birthdate and manipulating it*/
   var dayOfWeek = date.getDay();
   var name ='';
   if (gender == 'male'){
